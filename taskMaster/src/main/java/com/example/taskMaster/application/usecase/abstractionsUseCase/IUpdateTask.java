23f631b1d8b8398cfbc4c1afe9be1fa6337facac
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-09-26 22:38:43 -0300

    refactor: refactoring update logic

--- taskMaster/src/main/java/com/example/taskMaster/application/usecase/abstractionsUseCase/IUpdateTask.java
+++ taskMaster/src/main/java/com/example/taskMaster/application/usecase/abstractionsUseCase/IUpdateTask.java
@@ -1,8 +1,10 @@
 package com.example.taskMaster.application.usecase.abstractionsUseCase;
 
+import com.example.taskMaster.adapter.dto.UpdateInput;
+
 import java.util.Map;
 import java.util.UUID;
 
 public interface IUpdateTask {
-    String execute(UUID id, Map<String, String> field);
+    String execute(UUID id, UpdateInput updateInput);
 }
